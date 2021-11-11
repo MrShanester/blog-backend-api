3a7847ede956b788819ed020827c001ed1603c77
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-11 11:41:06 -0800

    Adds index and spruces up CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <img v-bind:src="post.image" v-bind:alt="post.title" />
+      <p>{{ post.body }}</p>
+      <p></p>
+      <p>---------------------------------</p>
+    </div>
   </div>
 </template>
 
+<style scoped>
+h1 {
+  text-align: center;
+  border: 12px solid;
+  border-image: linear-gradient(135deg, #ff0000 0%, #49ff33 25%, #3364ff 50%, #f6ff33 75%, #ff00a8 100%) 1;
+}
+
+img {
+  width: 250px;
+  height: 350px;
+}
+
+h2 {
+  font-style: italic;
+  border-top-style: dashed;
+  border-bottom-style: dashed;
+  border-left-style: solid;
+  border-right-style: solid;
+}
+
+.home {
+  margin: 0 auto;
+  padding: 10px 25px;
+  border: 3px solid;
+  border-image: linear-gradient(135deg, #ff0000 0%, #49ff33 25%, #3364ff 50%, #f6ff33 75%, #ff00a8 100%) 1;
+
+  background: rgb(228, 226, 226);
+}
+</style>
+
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome To My Blog",
+      posts: [],
     };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      axios.get("/posts").then((response) => {
+        this.posts = response.data;
+      });
+    },
   },
 };
 </script>
